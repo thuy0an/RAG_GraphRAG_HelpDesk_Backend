f25689b61d9ee9999859ae244b0e8381a95f0c71
phusomnia <tuanhphu2004thcs@gmail.com>
2026-04-11 07:54:54 +0700

    update packages.sh

---
+++ packages.sh
@@ -0,0 +1,44 @@
+# http
+fastapi
+scalar-fastapi
+lagom
+# database
+sqlacodegen
+sqlmodel
+pymysql
+mysqlclient
+aiomysql
+langchain-redis
+aiosqlite
+redis
+# langchain
+langchain
+langchain_community
+langchain-mistralai
+langchain-ollama
+langchain-core
+langchain-redis 
+rank-bm25
+langchain-classic
+langchain-neo4j
+# security
+bcrypt
+pyjwt
+# tools
+cloudinary
+python-multipart
+uuid6
+duckduckgo-search
+ddgs
+playwright
+trafilatura
+langchain-unstructured
+unstructured[pdf]
+langchain-text-splitters
+# loader
+granian
+redis
+nltk
+# crawler
+# UI
+streamlit
